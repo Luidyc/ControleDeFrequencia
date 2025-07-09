ecbcecc6cd58ffa02864896732cb8d52db6062ed
Luidyc <Luidyc@gmail.com>
2025-07-09 20:08:15 -0300

    Security Update

--- src/main/java/Development/team/ControleDeFrequencia/User/Infra/Security/SecurityFilter.java
+++ src/main/java/Development/team/ControleDeFrequencia/User/Infra/Security/SecurityFilter.java
@@ -26,14 +26,25 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        var login = tokenService.validateToken(token);
-        if(login != null ) {
-            UserEntity user = userRepository.findByLogin(login).orElseThrow(() -> new RuntimeException("User Not Found"));
-            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            var authentication = new UsernamePasswordAuthenticationToken(user,null, authorities);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        String path = request.getServletPath();
+
+        // Ignora rotas públicas
+        if (path.equals("/login")) {
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        var token = this.recoverToken(request);
+        if(token != null) {
+            var login = tokenService.validateToken(token);
+            if(login != null ) {
+                UserEntity user = userRepository.findByLogin(login).orElseThrow(() -> new RuntimeException("User Not Found"));
+                var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"));
+                var authentication = new UsernamePasswordAuthenticationToken(user,null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
         filterChain.doFilter(request,response);
     }
 
